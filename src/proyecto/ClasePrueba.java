cd09bedd6f2f611715c9eedcd3ad1196a7531172
The65Dude <marcobreda34@gmail.com>
2021-10-20 20:37:08 -0600

    V2

---
+++ src/proyecto/ClasePrueba.java
@@ -0,0 +1,12 @@
+package proyecto;
+import javax.swing.JOptionPane;
+/**
+ * @author Marco Zumbado Solorzano C18736
+ * @time 8:31:18 PM
+ * @date Oct 20, 2021
+ */
+public class ClasePrueba {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "hi");
+    }
+}
