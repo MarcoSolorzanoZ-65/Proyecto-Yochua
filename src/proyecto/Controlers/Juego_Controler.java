0b204e891d609056fe4cb6fef543b434ddb067b2
The65Dude <marcobreda34@gmail.com>
2021-11-23 12:12:43 -0600

    holis

--- src/proyecto/Controlers/Juego_Controler.java
+++ src/proyecto/Controlers/Juego_Controler.java
@@ -5,40 +5,48 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import proyecto.Vistas.Vista_Juego;
 import proyecto.DAO.Dao_MC;
+import proyecto.Lists.MultipleSelectionList;
+import proyecto.Lists.TrueOrFalseList;
+import proyecto.Lists.UniqueSelectionList;
+import proyecto.sampleClasses.MultipleSelection;
+import proyecto.sampleClasses.TrueOrFalse;
+import proyecto.sampleClasses.UniqueSelection;
 
 /**
  * @author Marco Zumbado Solorzano carne C18736
- * @date 2021-08-16 
+ * @date 2021-08-16
  * @time 10:13:20
-*/
-public class Juego_Controler implements ActionListener{
+ */
+public class Juego_Controler implements ActionListener {
 
-    private Vista_Juego vista; 
+    private Vista_Juego vista;
     private Dao_MC modelo;
-    
+
     public Juego_Controler(Vista_Juego vista, Dao_MC modelo) {
         this.vista = vista;
         this.modelo = modelo;
         this.vista.jb_mc.addActionListener(this);
-        this.vista.jB_Falso.addActionListener(this);
+        this.vista.jb_sc.addActionListener(this);
+        this.vista.jb_tf.addActionListener(this);
         iniciarVista();
     }
-    
-    public void actionPerformed(ActionEvent e){
-        if (e.getSource() == vista.jB_verdadero){
-            JOptionPane.showMessageDialog(vista, "La opcion seleccionada fue: verdadero");
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == vista.jb_tf) {
+
+        }
+
+        if (e.getSource() == vista.jb_mc) {
+
             vista.dispose();
         }
-        
-        if (e.getSource() == vista.jB_Falso){
-            JOptionPane.showMessageDialog(vista, "La opcion seleccionada fue: Falso");
+        if (e.getSource() == vista.jb_sc) {
+
             vista.dispose();
         }
     }
-    
-    public void iniciarVista(){  
-        Pregunta p = modelo.generarPreguntaRandom();
-        vista.jT_Enunciado.setText(p.getEnunciado());
+
+    public void iniciarVista() {
 
         vista.setVisible(true);
     }
